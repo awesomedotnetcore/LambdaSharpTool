350c5b1e05ac61d28d8c2d28f6410a90ffebffec
Steve G. Bjorg <steve.bjorg@gmail.com>
2018-11-02 13:13:21 -0700

    validate contributor and nuget builds

--- Docs/validate.sh
+++ Docs/validate.sh
@@ -24,14 +24,14 @@ if ! git diff-index --quiet HEAD -- Tests/; then
     exit 1
 fi
 
-# Bootstrap the λ# Environment
+# Setup λ# in Contributor Mode
 
 lash() {
     dotnet run -p $LAMBDASHARP/src/MindTouch.LambdaSharp.Tool/MindTouch.LambdaSharp.Tool.csproj -- $*
 }
 
 SUFFIX=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
-LAMBDASHARP_TIER=ReleaseTest$SUFFIX
+LAMBDASHARP_TIER=TestContrib$SUFFIX
 
 lash init
 if [ $? -ne 0 ]; then
@@ -99,3 +99,113 @@ if [ $? -ne 0 ]; then
 fi
 cd ..
 rm -rf Test$SUFFIX
+
+
+# Setup λ# in Nuget Mode
+cd src/MindTouch.LambdaSharp.Tool
+
+SUFFIX=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+LAMBDASHARP_TIER=TestNuget$SUFFIX
+
+
+# Install λ# as a global tool
+rm *.nupkg
+dotnet publish \
+    --configuration Release
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+dotnet pack \
+    --configuration Release \
+    --output ./
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+dotnet tool uninstall \
+    --global \
+    MindTouch.LambdaSharp.Tool \
+
+dotnet tool install \
+    --global \
+    --add-source $LAMBDASHARP/src/MindTouch.LambdaSharp.Tool/ \
+    MindTouch.LambdaSharp.Tool \
+    --version 0.4.0.1-RC1
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+cd ../..
+unset LAMBDASHARP
+
+dotnet lash init
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# Deploy the λ# Demos
+
+dotnet lash deploy \
+    Demos/Demo \
+    Demos/BadModule
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# Deploy all λ# Sample Modules
+
+dotnet lash build \
+    Samples/AlexaSample \
+    Samples/ApiSample \
+    Samples/DynamoDBSample \
+    Samples/KinesisSample \
+    Samples/MacroSample \
+    Samples/S3Sample \
+    Samples/ScheduleSample \
+    Samples/SlackCommandSample \
+    Samples/SnsSample \
+    Samples/SqsSample
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+dotnet lash deploy \
+    Samples/AlexaSample/bin/manifest.json \
+    Samples/ApiSample/bin/manifest.json \
+    Samples/DynamoDBSample/bin/manifest.json \
+    Samples/KinesisSample/bin/manifest.json \
+    Samples/MacroSample/bin/manifest.json \
+    Samples/S3Sample/bin/manifest.json \
+    Samples/ScheduleSample/bin/manifest.json \
+    Samples/SlackCommandSample/bin/manifest.json \
+    Samples/SnsSample/bin/manifest.json \
+    Samples/SqsSample/bin/manifest.json
+
+# Create a Default λ# Module and Deploy it
+
+# mkdir Test$SUFFIX
+mkdir Test$SUFFIX
+cd Test$SUFFIX
+dotnet lash new module MyModule
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+dotnet lash new function MyFirstFunction
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+dotnet lash new function --language javascript MySecondFunction
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+dotnet lash deploy
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+cd ..
+rm -rf Test$SUFFIX
+
+dotnet tool uninstall \
+    --global \
+    MindTouch.LambdaSharp.Tool \
