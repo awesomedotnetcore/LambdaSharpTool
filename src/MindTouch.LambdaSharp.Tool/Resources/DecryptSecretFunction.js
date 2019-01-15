6ee0936f684fa96d13a03169a56cddcf7c076b42
Steve G. Bjorg <steve.bjorg@gmail.com>
2019-01-14 16:41:13 -0800

    modernized the decrypt function a bit

--- src/MindTouch.LambdaSharp.Tool/Resources/DecryptSecretFunction.js
+++ src/MindTouch.LambdaSharp.Tool/Resources/DecryptSecretFunction.js
@@ -1,51 +1,12 @@
-// TODO: review and condense
-
-'use strict';
-
-var AWS = require('aws-sdk');
-var https = require('https');
-var url = require('url');
-var kms = new AWS.KMS();
+const AWS = require('aws-sdk');
+const https = require('https');
+const URL = require('url').URL;
+const kms = new AWS.KMS();
 
 var logInfo = message => console.log('*** INFO: ' + message);
 var logError = message => console.log('*** ERROR: ' + message);
 
-var send = function(event, context, status, data, reason) {
-    var body = {
-        Status: status,
-        Reason: (status == 'FAILED') ? (reason || 'operation failed') : '',
-        PhysicalResourceId: 'decrypted:' + event.LogicalResourceId,
-        StackId: event.StackId,
-        RequestId: event.RequestId,
-        LogicalResourceId: event.LogicalResourceId,
-        NoEcho: true,
-        Data: '...'
-    };
-    logInfo('response: ' + JSON.stringify(body));
-    body.Data = data;
-    var payload = JSON.stringify(body);
-    var parsedUrl = url.parse(event.ResponseURL);
-    var request = https.request({
-        hostname: parsedUrl.hostname,
-        port: 443,
-        path: parsedUrl.path,
-        method: 'PUT',
-        headers: {
-            'content-type': '',
-            'content-length': payload.length
-        }
-    }, function(response) {
-        context.done();
-    });
-    request.on('error', function(error) {
-        logError('send(..) failed executing https.request(..): ' + error);
-        context.done();
-    });
-    request.write(payload);
-    request.end();
-};
-
-exports.handler = function(event, context) {
+exports.handler = (event, context) => {
     try {
         logInfo('request: ' + JSON.stringify(event));
         switch(event.RequestType) {
@@ -53,15 +14,15 @@ exports.handler = function(event, context) {
         case 'Update':
             kms.decrypt({
                 CiphertextBlob: new Buffer(event.ResourceProperties.Ciphertext, 'base64')
-            }, function(err, result) {
+            }, (err, result) => {
                 if(err) {
                     logError('decrypt failed: ' + JSON.stringify(err));
                     send(event, context, 'FAILED', null, err.message);
-                    return;
+                } else {
+                    send(event, context, 'SUCCESS', {
+                        Plaintext: result.Plaintext.toString('utf8')
+                    });
                 }
-                send(event, context, 'SUCCESS', {
-                    Plaintext: result.Plaintext.toString('utf8')
-                });
             });
             break;
         case 'Delete':
@@ -76,3 +37,38 @@ exports.handler = function(event, context) {
         send(event, context, 'FAILED', null, 'internal error');
     }
 };
+
+function send(event, context, status, data, reason) {
+    var body = {
+        Status: status,
+        Reason: (status == 'FAILED') ? (reason || 'operation failed') : '',
+        PhysicalResourceId: 'decrypted:' + event.LogicalResourceId,
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        NoEcho: true,
+        Data: '...'
+    };
+    logInfo('response: ' + JSON.stringify(body));
+    body.Data = data;
+    const payload = JSON.stringify(body);
+    const parsedUrl = url.parse(event.ResponseURL);
+    const request = https.request({
+        hostname: parsedUrl.hostname,
+        port: 443,
+        path: parsedUrl.path,
+        method: 'PUT',
+        headers: {
+            'content-type': '',
+            'content-length': payload.length
+        }
+    }, () => {
+        context.done();
+    });
+    request.on('error', error => {
+        logError('send(..) failed executing https.request(..): ' + error);
+        context.done();
+    });
+    request.write(payload);
+    request.end();
+}
